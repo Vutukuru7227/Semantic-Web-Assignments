1db9c7fd17afb37a948b99a891f927f2ba61a9cf
Ram Anand Vutukuru <anand.vootukuru@gmail.com>
2017-10-03 16:30:22 -0500

    Lab 3

--- Lab 3/src/lab3_2.java
+++ Lab 3/src/lab3_2.java
@@ -1,5 +1,7 @@
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.util.Date;
+
 import org.apache.jena.query.*;
 
 import org.apache.jena.query.QueryFactory;
@@ -12,49 +14,61 @@ public class lab3_2 {
 		
 		Model model = ModelFactory.createDefaultModel();
 		InputStream input = FileManager.get().open("Monterey.rdf");
-		System.out.println(input);
+		//System.out.println(input);
+		Date beforeReading = new Date();
+		//System.out.println(beforeReading.getTime());
+		
 		model.read(input, "");
 		
-		String sparqlQuery = "prefix mon:<http://urn.monterey.org/incidents#>"
-				+ "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-				+ "prefix bb:<http://blackbook.com/terms#>"
-				+ "prefix geo:<http://www.w3.org/2003/01/geo/wgs84_pos#>"
-				+ "prefix vCard:<http://www.w3.org/2001/vcard-rdf/3.0#>"
-				+ "prefix dc:<http://purl.org/dc/elements/1.1/>"
-				+ "prefix event:<http://ebiquity.umbc.edu/ontology/event.owl#>"
-				+ "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>"
-				+ "select ?predicate ?object "
-				+ "where{"
-				+ "mon:incident1 ?predicate ?object ."
-				+ "FILTER(!isBlank(?object))\n"
-				+ "}";
+		Date afterReading = new Date();
+		System.out.println("Load of Montery.rdf took "+(afterReading.getTime() - beforeReading.getTime())/(float)1000+ " seconds");
+		
+//		String sparqlQuery = "prefix mon:<http://urn.monterey.org/incidents#>"
+//				+ "prefix rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+//				+ "prefix bb:<http://blackbook.com/terms#>"
+//				+ "prefix geo:<http://www.w3.org/2003/01/geo/wgs84_pos#>"
+//				+ "prefix vCard:<http://www.w3.org/2001/vcard-rdf/3.0#>"
+//				+ "prefix dc:<http://purl.org/dc/elements/1.1/>"
+//				+ "prefix event:<http://ebiquity.umbc.edu/ontology/event.owl#>"
+//				+ "prefix rdfs:<http://www.w3.org/2000/01/rdf-schema#>"
+//				+ "select ?predicate ?object "
+//				+ "where{"
+//				+ "mon:incident1 ?predicate ?object ."
+//				+ "FILTER(!isBlank(?object))"
+//				+ "}"
+//				+ "UNION{"
+//				+ "{"
+//				+ ""
+//				+ "}";
 		
 		
 //		prefix mon:<http://urn.monterey.org/incidents#>
 //
-//			SELECT ?pred ?obj WHERE {
-//			                           {      
-//			                              mon:incident1 ?pred ?obj .
-//			                              FILTER (!isBlank (?obj))
-//			                           }
-//			                           UNION 
-//			                              {
-//			                                 {        
-//			                                      mon:incident1 ?p1 ?blankNode  .
-//			                                      FILTER (isBlank (?blankNode)).
-//			                                      ?blankNode ?pred ?obj .
-//			                                      FILTER (!isBlank (?obj)) .
-//			                                  }
-//			                                  UNION
-//			                                       {
-//			                                           mon:incident1 ?p1 ?blankNode  .
-//			                                           FILTER (isBlank (?blankNode)).
-//			                                           ?blankNode ?p2 ?blankNode2 .
-//			                                           FILTER (isBlank (?blankNode2)).
-//			                                           ?blankNode2 ?pred ?obj .
-//			                                       }
-//			                               }
-//			                            }
+		String sparqlQuery = 	"PREFIX mon:<http://urn.monterey.org/incidents#>"
+		+ "SELECT ?pred ?obj WHERE {"
+		+ "{" + 
+		"			                              mon:incident1 ?pred ?obj .\n" + 
+		"			                              FILTER (!isBlank (?obj))\n" + 
+		"			                           }\n" + 
+		"			                           UNION \n" + 
+		"			                              {\n" + 
+		"			                                 {        \n" + 
+		"			                                      mon:incident1 ?p1 ?blankNode  .\n" + 
+		"			                                      FILTER (isBlank (?blankNode)).\n" + 
+		"			                                      ?blankNode ?pred ?obj .\n" + 
+		"			                                      FILTER (!isBlank (?obj)) .\n" + 
+		"			                                  }\n" + 
+		"			                                  UNION\n" + 
+		"			                                       {\n" + 
+		"			                                           mon:incident1 ?p1 ?blankNode  .\n" + 
+		"			                                           FILTER (isBlank (?blankNode)).\n" + 
+		"			                                           ?blankNode ?p2 ?blankNode2 .\n" + 
+		"			                                           FILTER (isBlank (?blankNode2)).\n" + 
+		"			                                           ?blankNode2 ?pred ?obj .\n" + 
+		"			                                       }\n" + 
+		"			                               }\n" + 
+		"			                            }";
+			                           
 
 		
 		
